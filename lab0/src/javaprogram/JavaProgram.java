3edb044949dc1eaabea65b8f4ef3d3568fc48425
HP <HP@10.102.22.3>
2023-10-16 21:10:46 +0800

    First Commit

---
+++ lab0/src/javaprogram/JavaProgram.java
@@ -0,0 +1,15 @@
+package javaprogram;
+
+public class JavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("Welcome");
+	System.out.println("");
+	System.out.println("Java");
+	System.out.println("Programming");
+
+	}
+
+}
