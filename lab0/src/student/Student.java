f1668083d10085841eadcf8e2b0962252a79d10a
HP <HP@10.102.22.3>
2023-10-16 18:04:34 +0800

    Student

---
+++ lab0/src/student/Student.java
@@ -0,0 +1,13 @@
+package student;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Nor Fadira Binti Hasim");
+		System.out.println("216086");
+		System.out.println("Kota Bharu");
+	}
+
+}
